ee11aeb64567291fee09fc0fc666242836561592
babamunu <ishankshp@gmail.com>
2025-05-18 20:13:02 +0530

    initiate commit for PowerCalc

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sumrwell.powercalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface {
+                Text("PowerCalc App")
+            }
+        }
+    }
+}
